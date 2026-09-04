060edaa39a1e27da4cb86da02aa0b3f4f14f9c94
scavengerui <ksdhanushreddy@gmail.com>
2026-09-05 00:59:38 +0530

    fix(billing): populate upgrade route entrypoints for Next.js production build

---
+++ src/app/subscriptions-billing/upgrade/page.tsx
@@ -0,0 +1,10 @@
+import { AdminShell } from '@/components/layout/AdminShell';
+import { UpgradePlanView } from '@/components/billing/UpgradePlanView';
+
+export default function UpgradePlanPage() {
+  return (
+    <AdminShell>
+      <UpgradePlanView />
+    </AdminShell>
+  );
+}
